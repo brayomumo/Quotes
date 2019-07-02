53cbfe0b9444dda77d35684ac785b439a5530fc0
brian <brayomumo>
2019-07-02 08:09:11 +0300

    import form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
+    QuotesFormComponent,
   ],
   imports: [
     BrowserModule,
