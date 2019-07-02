3ce22ca0579dcef5b517cbaaedd88d89b8c7f551
brian <brayomumo>
2019-07-02 12:48:31 +0300

    Add function

---
+++ src/app/quotes-form/quotes-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quotes-form',
+  templateUrl: './quotes-form.component.html',
+  styleUrls: ['./quotes-form.component.css']
+})
+export class QuotesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
